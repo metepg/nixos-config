4356d3deca85f3610087bf8244cb527262021157
Mete Guneysel <52155024+metepg@users.noreply.github.com>
2026-01-08 14:34:09 +0200

    Modify i3 so it follows window after moving it

--- home/i3.nix
+++ home/i3.nix
@@ -65,16 +65,16 @@ in
         "${modifier}+0" = "workspace number 10";
 
         # Move windows
-        "${modifier}+Shift+1" = "move container to workspace number 1";
-        "${modifier}+Shift+2" = "move container to workspace number 2";
-        "${modifier}+Shift+3" = "move container to workspace number 3";
-        "${modifier}+Shift+4" = "move container to workspace number 4";
-        "${modifier}+Shift+5" = "move container to workspace number 5";
-        "${modifier}+Shift+6" = "move container to workspace number 6";
-        "${modifier}+Shift+7" = "move container to workspace number 7";
-        "${modifier}+Shift+8" = "move container to workspace number 8";
-        "${modifier}+Shift+9" = "move container to workspace number 9";
-        "${modifier}+Shift+0" = "move container to workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1; workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2; workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3; workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4; workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5; workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6; workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7; workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8; workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9; workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10; workspace number 10";
       };
 
       # Rofi launcher
